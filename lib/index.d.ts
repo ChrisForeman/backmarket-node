ed9f23ae0cae29b6ec611a24e4a4d4552a9c448a
Chris <kissthecoder@gmail.com>
2021-11-29 12:24:07 -0500

    Built changes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,4 +2,5 @@ import { BackMarketAPI } from './api';
 export default BackMarketAPI;
 import { CatalogFields, UpdateListingFields } from "./interal-types";
 import { BackMarketAPIInterface } from './api-interface';
-export { CatalogFields, UpdateListingFields, BackMarketAPIInterface };
+import { CountryCode } from './enums';
+export { CatalogFields, UpdateListingFields, BackMarketAPIInterface, CountryCode };
